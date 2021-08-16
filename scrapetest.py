4d4973ef2d37892c51ec8fbef77c7c2970b6e7bd
AA444497 <70088615+AA444497@users.noreply.github.com>
2021-08-16 09:27:20 -0700

    Update scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -4,3 +4,11 @@ web = 'https://sportsbook.draftkings.com/leagues/football/88670561?category=game
 path = '/Users/Arjuna/Downloads/chromedriver' #introduce your file's path inside '...'
 driver = webdriver.Chrome(path)
 driver.get(web)
+teams = []
+x12 = [] #3-way
+odds_events = []
+
+box = driver.find_element_by_xpath('sportsbook-league-page dkbetslip')
+
+sport_title = box.find_elements_by_class_name('sportsbook-breadcrumb__end')
+
